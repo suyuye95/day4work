edc64883283c4a461b2eac58313b094998be76dc
syy <446390252@qq.com>
2019-02-15 20:10:35 +0800

    项目完成

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCss = require('gulp-clean-css');
+var server = require('gulp-webserver');
+var path = require('path');
+var fs = require('fs');
+var url = require('url');
+var data = require('./data.json');
+var uglify = require('gulp-uglify');
 
 //编译sass
 gulp.task('minSass', function() {
@@ -10,7 +16,55 @@ gulp.task('minSass', function() {
         .pipe(gulp.dest('./css/'))
 })
 
-//监听scss文件变化编译cs
+//起服务
+gulp.task('server', function() {
+        return gulp.src('../src/')
+            .pipe(server({
+                port: 8080,
+                open: true,
+                middleware: function(req, res, next) {
+                    var pathname = url.parse(req.url).pathname;
+                    if (pathname == '/favicon.ico') {
+                        return res.end()
+                    } else if (pathname == '/data') {
+                        res.end(JSON.stringify({ code: 1, msg: data }));
+                    } else {
+                        pathname = pathname == '/' ? 'index.html' : pathname;
+                        res.end(fs.readFileSync(path.join(__dirname, pathname)));
+                    }
+                }
+            }))
+
+    })
+    //监听scss文件变化编译cs
 gulp.task('watch', function() {
     gulp.watch('./scss/*.scss', gulp.series('minSass'))
 })
+gulp.task('dev', gulp.series('server', 'watch'))
+
+//放到上线上
+//拷贝css 到bulid里面
+gulp.task('copyCss', function() {
+        return gulp.src('./css/*.css')
+            .pipe(gulp.dest('../bulid/css/'))
+    })
+    //拷贝iconCss
+gulp.task('copyIcon', function() {
+    return gulp.src('./icon/*.css')
+        .pipe(gulp.dest('../bulid/icon/'))
+})
+
+//压缩自己的js文件并且拷贝
+gulp.task('minJs', function() {
+    gulp.src('./js/yjs/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('../bulid/js/yjs/'))
+})
+
+//拷贝所有js文件
+gulp.task('copyJs', function() {
+    gulp.src(['./js/**/*.js', '!./js/yjs/*.js'])
+        .pipe(gulp.dest('../bulid/js/'))
+})
+
+gulp.task('bulidDev', gulp.series('copyCss', 'copyIcon', 'copyJs', 'minJs'))
--- src/js/yjs/index.js
+++ src/js/yjs/index.js
@@ -1,3 +1,21 @@
 var mySwiper = new Swiper('.swiper-container', {
     autoplay: true
 })
+var list = document.querySelector('.list');
+$.ajax({
+    url: "/data",
+    dataType: "json",
+    success: function(msg) {
+        //console.log(data.msg);
+        var data = msg.msg.list;
+        //console.log(data);
+        if (msg.code == 1) {
+            var str = '';
+            data.forEach(function(v, i) {
+                str += '<dl><dt><img src="' + v.imgs + '" alt=""></dt><dd><h4>' + v.title + '</h4><span>' + v.price + '</span></dd></dl>'
+            })
+            list.innerHTML = str;
+        }
+    }
+})
+var myBScroll = new BScroll('.section');
